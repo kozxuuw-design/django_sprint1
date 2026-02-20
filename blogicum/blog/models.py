02496a7240dcf4f346429c769228adfeaaf2a915
tim8006 <romanov.timofey124@gmail.com>
2026-02-20 17:03:26 +0700

    hjvh

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
-
-class Post(models.Model):
-    title = models.CharField(max_length=256)
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
-    category = models.CharField(max_length=256)
-
-    def __str__(self):
-        return self.title
+# Create your models here.
